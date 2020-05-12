feeda848301f99be826bac5905d34fb52179a238
Ubuntu <ubuntu@ip-172-31-49-72.ec2.internal>
2020-05-12 01:16:40 +0000

    MPI hosts

---
+++ gen_mpi_hosts.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def main():
+    procs_per_server = 8
+    outfile = 'servers/mpi_hosts.txt'
+    servers = []
+    with open('servers/servers.txt', 'r') as f:
+        servers = [l.strip() for l in f.readlines()]
+
+    with open(outfile, 'w') as f:
+        for s in servers:
+            f.write(s + ':' + str(procs_per_server) + '\n')
+        
+if __name__=='__main__':
+    main()
